df80ac27aa7a5c969d4e1fc2b90a537eea7f6e61
Ahmed Hany <ahmed_hany19@outlook.com>
2022-09-19 20:15:33 +0200

    Using Memoization

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder_API";
+import _ from "lodash";
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
@@ -6,8 +7,15 @@ export const fetchPosts = () => async (dispatch) => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUsers = (id) => async (dispatch) => {
+export const fetchUsers = (id) => (dispatch) => {
+  // This method used to prevent multi API calls for the same user by memorizing the result
+  // The downside of this method is that it can't update the data even if it changes on the server
+  _fetchUsers(id, dispatch);
+};
+
+// The _ mark at the begging means its a privet func, so don't call it unless you know what you're doing
+const _fetchUsers = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-};
+});
